ed8e0d5679307fb05907edfb102df470fd67e166
Shadyaobuya <shadyaobuyagard@gmail.com>
2021-03-19 16:44:38 +0300

    PythonStrings

---
+++ assignmentStrings.py
@@ -0,0 +1,9 @@
+first_name="Shadya"
+last_name="Obuya"
+year_of_birth=1999
+country="Kenya"
+fees=400000
+payment_made=10000
+school="akiraChix"
+
+print(f"Hello {first_name} {last_name}, Welcome to {school.upper()} from {country.upper()}.\nYour fee balance is {fees-payment_made} and you are {2021- year_of_birth} years old")
