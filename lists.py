68fdf11c0a51990efee05ff14a2687a3fa4721a2
Shadyaobuya <shadyaobuyagard@gmail.com>
2021-03-27 23:24:58 +0300

    Lists

---
+++ lists.py
@@ -0,0 +1,30 @@
+import sys
+countries=["Kenya","Uganda","Rwanda", "South Sudan"]
+print(countries[1:3])
+
+
+newCountries=["Burundi","Tanzania"]
+countries.extend(newCountries)
+print (countries)
+
+countries.sort(reverse=True)
+print(countries)
+
+
+a=[[1,2,3],[4,5,6],[7,8,9]]
+b=[]
+for i in a:
+    for number in i:
+            b.append(number)
+
+print(b)
+
+c=[val%2 for val in b ]
+print(c)
+
+
+
+        
+
+    
+    
---
+++ stringmethodsexercise.py
@@ -0,0 +1,11 @@
+s="I love the python world"
+print(s[11:17].upper())
+
+
+
+if not s.index("") > 4:
+    if s.index(" "):
+        ++s.index("")
+        s.title().index("")
+        print(++s.title().index(""))
+        
